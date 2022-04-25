16620d9d7deea6abe7faca205d6c80b1f449cf09
Braulio Antonio Hernandez <braulio.antonio@consiss.com>
2022-04-25 10:09:47 -0500

    Initial

---
+++ pages/inspire.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-row>
+    <v-col class="text-center">
+        <dropzone id="foo" ref="el" :options="options" :destroyDropzone="true" v-on:vdropzone-sending="sendingEvent"></dropzone>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+import Dropzone from 'nuxt-dropzone'
+import 'nuxt-dropzone/dropzone.css'
+ 
+export default {
+  components: {
+    Dropzone
+  },
+  data() {
+    return {
+      options: {
+        url: "http://httpbin.org/anything"
+      }
+    }
+  },
+  methods:{
+    sendingEvent(file, xhr, formData){
+      formData.append("uploads",file,file.name)
+      this.$refs.el.removeAllFiles()
+    }
+  },
+  mounted() {
+    const instance = this.$refs.el.dropzone
+  }
+}
+</script>
